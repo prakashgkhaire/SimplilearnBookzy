e19a281a60ba7cc94b076ee1e4ac5fd727b58815
Prakash G Khaire <prakashgkhaire@gmail.com>
2022-04-03 06:55:33 +0000

    Add files via upload

---
+++ simplilearnbookzy/src/main/java/com/simplilearn/simplilearnbookzy/SimplilearnbookzyApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.simplilearnbookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnbookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnbookzyApplication.class, args);
+	}
+
+}
---
+++ simplilearnbookzy/src/test/java/com/simplilearn/simplilearnbookzy/SimplilearnbookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.simplilearnbookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnbookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
